0f5e5ed8c1b29556194da181e625f10000b4da05
Artemf29 <57762801+artemf29@users.noreply.github.com>
2021-05-04 22:47:52 +0300

    create To

--- src/main/java/com/artemf29/core/to/VoteTo.java
+++ src/main/java/com/artemf29/core/to/VoteTo.java
@@ -1,4 +1,49 @@
 package com.artemf29.core.to;
 
-public class VoteTo {
+import java.beans.ConstructorProperties;
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class VoteTo extends BaseTo implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private final LocalDate date;
+
+    private final int userId;
+
+    private final int restId;
+
+    @ConstructorProperties({"id", "date", "userId", "restId"})
+    public VoteTo(Integer id, LocalDate date, int userId, int restId) {
+        super(id);
+        this.date = date;
+        this.userId = userId;
+        this.restId = restId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VoteTo voteTo = (VoteTo) o;
+        return userId == voteTo.userId && restId == voteTo.restId && date.equals(voteTo.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, userId, restId);
+    }
+
+    @Override
+    public String toString() {
+        return "VoteTo{" +
+                "id=" + id +
+                ", date=" + date +
+                ", userId=" + userId +
+                ", restId=" + restId +
+                '}';
+    }
 }
